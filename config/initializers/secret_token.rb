b0c11da24d40c295cf19437182af9cff608d0624
pyemkey <py.emkey@gmail.com>
2014-02-08 12:44:59 +0100

    Depot Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '83a0c6510d2eaf30d2a1b1f47ca012a1a148a88c3b4601239fc257ae0ec48ffcdef98b8156d5ba0e71372c1cab65fd9820ed5d7bbcb02ab7179e63f42d19796b'
