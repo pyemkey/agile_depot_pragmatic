90092b8b10dd364d6a96061d5599f2d239ca0c20
pyemkey <py.emkey@gmail.com>
2014-02-09 15:42:15 +0100

    Catalog display

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  helper_method :date_loaded
   protect_from_forgery with: :exception
+
+  def date_loaded
+  	t = Time.now
+  	t.strftime("%F %H:%M")
+  end
 end
