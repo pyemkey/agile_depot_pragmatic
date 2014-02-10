ceb406fb5f6e76aaa3fb5baaddd9138c01824f15
pyemkey <py.emkey@gmail.com>
2014-02-10 22:11:20 +0100

    Create cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,24 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  helper_method :date_loaded
+  helper_method :date_loaded, :counter
   protect_from_forgery with: :exception
 
   def date_loaded
   	t = Time.now
   	t.strftime("%F %H:%M")
   end
+
+  def counter
+  	session[:counter] ||= 0
+  end
+
+  def increment_counter
+			counter			
+			session[:counter] += 1
+	end
+
+	def reset_counter
+		session[:counter] = nil
+	end
 end
