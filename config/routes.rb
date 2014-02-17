67f6fc4cad4f5e1bb9b71139a41c4ca2364cfae5
pyemkey <py.emkey@gmail.com>
2014-02-17 12:45:55 +0100

    Internationalization

--- config/routes.rb
+++ config/routes.rb
@@ -8,17 +8,18 @@ Depot::Application.routes.draw do
   get "admin/index"
   resources :users
 
-  resources :orders
-
-  resources :line_items
-
-  resources :carts
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root "store#index", as: 'store', via: :all
+  end
 
   get "store/index"
   resources :products do
     get :who_bought, on: :member
   end
-  root "store#index", as: 'store'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
